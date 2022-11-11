704ec15a429f775a7bee2df7f8b0ecdbb48b6f53
Domagoj Mahnet <domagoj.mahnet@king-ict.cloud>
2022-11-11 10:51:05 +0100

    Debouncing

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
           style="height: 30px"
           label="Search"
           placeholder="Item name"
-          v-model="store.searchString"
+          v-on:input="debounceSearch"
         ></v-text-field>
         <v-btn elevation="2" class="btn" @click="addItem()"> Add Item </v-btn>
         <v-btn elevation="2" class="btn" @click="store.archiveAll()"> Archive all </v-btn>
@@ -26,6 +26,7 @@ import { useTodoStore } from "@/store";
 import { Item } from "./models/Item";
 import { ItemStatusEnum } from "./models/ItemStatusEnum";
 import { storeToRefs } from "pinia";
+import _ from "lodash";
 
 export default defineComponent({
   setup() {
@@ -42,9 +43,14 @@ export default defineComponent({
       store.addItem(element);
     }
 
+    const debounceSearch = _.debounce((value: any) => {
+      store.searchString = value;
+    }, 1000);
+
     return {
       store,
-      addItem
+      addItem,
+      debounceSearch
     };
   },
   name: "App",
